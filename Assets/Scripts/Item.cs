d5d8a068c7d4adf176179f22360187846b348831
Kamen6666 <mymusic990610@163.com>
2020-11-19 14:32:35 +0800

    最新版本 都更新下 11.19 14：32  ---赵雨轩

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+   public int ID { get; set; }
+   public string Name { get; set; }
+   public ItemType itemType { get; set; }
+   public Quality quality { get; set; }
+   public string Description { get; set; }
+   public int BuyPrice { get; set; }
+   public int SellPrice { get; set; }
+   public string Sprite { get; set; }
+
+   public Item(int id,string name,ItemType itemType,Quality quality,string description,int buyPrice,int sellPrice,string sprite)
+   {
+      this.ID = id;
+      this.Name = name;
+      this.itemType = itemType;
+      this.quality = quality;
+   }
+
+   public enum ItemType
+   {
+      Consumable,
+      Equioment,
+      Weapon,
+      Materail
+   }
+   /// <summary>
+   /// 品质
+   /// </summary>
+   public enum Quality
+   {
+      Common,
+      Uncomman,
+      /// <summary>
+      /// 史诗
+      /// </summary>
+      Epic,
+      /// <summary>
+      /// 传说
+      /// </summary>
+      Legendary
+   }
+}
